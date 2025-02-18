608b90d166a0d55d717a4ec627c3255580f25564
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2025-02-18 16:28:36 -0300

    Add files via upload

---
+++ 24 - 04 - 04 (1).c
@@ -0,0 +1,12 @@
+/*int main() (Soma dos numeros de PA razão 1, com An = n)
+{
+    int i,n,soma;
+    printf("Digite um número: ");
+    scanf("%d", &n);
+    for(i=0; i<n; i++){
+        soma = soma + (i+1);
+    }
+    printf("A soma dele com seus antecessores em cadeia eh: %d", soma);
+    return 0;
+}
+*/
